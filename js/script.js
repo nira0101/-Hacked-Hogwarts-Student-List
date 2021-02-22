49ab4bd9b96f1f3fe160b3f9aa7aad294a2fac37
nirajan <nira0101@stud.kea.dk>
2021-02-22 20:22:50 +0100

    added filter, added sort

--- js/script.js
+++ js/script.js
@@ -5,18 +5,10 @@ window.addEventListener("DOMContentLoaded", init);
 let allStudents = [];
 
 //prototype for all students
-const Student = {
-  fname: "",
-  mname: "",
-  lname: "",
-  sHouse: "",
-  gender: "",
-  responsibilities: "",
-};
 
 function init() {
   fetchData();
-  filterButton();
+  /*   filterButton(); */
 }
 async function fetchData() {
   console.log(fetchData);
@@ -26,84 +18,97 @@ async function fetchData() {
   const jsonData = await response.json();
 
   //when loaded prepare data object
-  prepareObject(jsonData);
+  prepareData(jsonData);
 }
 
-function prepareObject(jsonData) {
-  jsonData.forEach((jsonObject) => {
-    //Create new object with cleaned data - and store that in the allStudent array
-    const studentData = Object.create(Student);
-    console.log(studentData);
+function prepareData(jsonData) {
+  allStudents = jsonData.map(prepareObject);
+  displayList(allStudents);
+  buttonClicked();
+}
 
-    //split the full details into parts
-    const infoSplit = jsonObject.fullname.trim().split(" ");
-    const stuFName = jsonObject.fName;
-    const stuMName = jsonObject.mName;
-    const stuLName = jsonObject.lName;
-    const stuHouse = jsonObject.house.trim();
-    const stuGender = jsonObject.gender;
-    //console.log(infoSplit);
-    //console.log(stuGender);
-    //console.log(stuHouse);
-    if (infoSplit.length == 1) {
-      studentData.fName = infoSplit[0];
+function prepareObject(stu) {
+  console.log(stu);
+  const Student = {
+    fname: "",
+    mname: "",
+    lname: "",
+    house: "",
+    gender: "",
+  };
 
-      //to change first name in uppercase
-      studentData.fName =
-        studentData.fName[0].toUpperCase() +
-        studentData.fName.substring(1).toLowerCase();
-    } else if (infoSplit.length == 2) {
-      studentData.fName = infoSplit[0];
+  const studentData = Object.create(Student);
+  console.log(studentData);
 
-      //to change first name in uppercase
-      studentData.fName =
-        studentData.fName[0].toUpperCase() +
-        studentData.fName.substring(1).toLowerCase();
+  //split the full details into parts
+  const infoSplit = stu.fullname.trim().split(" ");
+  const stuFName = stu.fName;
+  const stuMName = stu.mName;
+  const stuLName = stu.lName;
+  const stuHouse = stu.house.trim();
+  const stuGender = stu.gender;
+  //console.log(infoSplit);
+  //console.log(stuGender);
+  //console.log(stuHouse);
+  if (infoSplit.length == 1) {
+    studentData.fName = infoSplit[0];
 
-      //change last name to uppercase
-      studentData.lName = infoSplit[1];
-      studentData.lName =
-        studentData.lName[0].toUpperCase() +
-        studentData.lName.substring(1).toLowerCase();
-    } else if (infoSplit.length == 3) {
-      studentData.fName = infoSplit[0];
+    //to change first name in uppercase
+    studentData.fName =
+      studentData.fName[0].toUpperCase() +
+      studentData.fName.substring(1).toLowerCase();
+  } else if (infoSplit.length == 2) {
+    studentData.fName = infoSplit[0];
 
-      //to change first name in uppercase
-      studentData.fName =
-        studentData.fName[0].toUpperCase() +
-        studentData.fName.substring(1).toLowerCase();
+    //to change first name in uppercase
+    studentData.fName =
+      studentData.fName[0].toUpperCase() +
+      studentData.fName.substring(1).toLowerCase();
 
-      //to change mid name to upper case
-      studentData.mName = infoSplit[1];
-      studentData.mName =
-        studentData.mName[0].toUpperCase() +
-        studentData.mName.substring(1).toLowerCase();
+    //change last name to uppercase
+    studentData.lName = infoSplit[1];
+    studentData.lName =
+      studentData.lName[0].toUpperCase() +
+      studentData.lName.substring(1).toLowerCase();
+  } else if (infoSplit.length == 3) {
+    studentData.fName = infoSplit[0];
 
-      //change last name to uppercase
-      studentData.lName = infoSplit[2];
-      studentData.lName =
-        studentData.lName[0].toUpperCase() +
-        studentData.lName.substring(1).toLowerCase();
-    }
-    //studentData.name = infoSplit[0] + " " + infoSplit[1] + " " + infoSplit[2];
-    studentData.gender = stuGender;
-    studentData.gender =
-      studentData.gender[0].toUpperCase() +
-      studentData.gender.substring(1).toLowerCase();
+    //to change first name in uppercase
+    studentData.fName =
+      studentData.fName[0].toUpperCase() +
+      studentData.fName.substring(1).toLowerCase();
 
-    studentData.house = stuHouse;
-    studentData.house =
-      studentData.house[0].toUpperCase() +
-      studentData.house.substring(1).toLowerCase();
+    //to change mid name to upper case
+    studentData.mName = infoSplit[1];
+    studentData.mName =
+      studentData.mName[0].toUpperCase() +
+      studentData.mName.substring(1).toLowerCase();
 
-    studentData.fName = studentData.fName;
+    //change last name to uppercase
+    studentData.lName = infoSplit[2];
+    studentData.lName =
+      studentData.lName[0].toUpperCase() +
+      studentData.lName.substring(1).toLowerCase();
+  }
+  //studentData.name = infoSplit[0] + " " + infoSplit[1] + " " + infoSplit[2];
+  studentData.gender = stuGender;
+  studentData.gender =
+    studentData.gender[0].toUpperCase() +
+    studentData.gender.substring(1).toLowerCase();
 
-    studentData.mName = studentData.mName;
+  studentData.house = stuHouse;
+  studentData.house =
+    studentData.house[0].toUpperCase() +
+    studentData.house.substring(1).toLowerCase();
 
-    studentData.lName = studentData.lName;
+  studentData.fName = studentData.fName;
 
-    //to show letters afetr hyphen and "" in uppercase
-    /* if (studentData.mName.includes('"')) {
+  studentData.mName = studentData.mName;
+
+  studentData.lName = studentData.lName;
+
+  //to show letters afetr hyphen and "" in uppercase
+  /* if (studentData.mName.includes('"')) {
       studentData.mName =
         studentData.mName[1].toUpperCase() +
         studentData.mName
@@ -111,7 +116,7 @@ function prepareObject(jsonData) {
           .toLowerCase();
     } */
 
-    /*     if (studentData.lName.includes("-")) {
+  /*     if (studentData.lName.includes("-")) {
       studentData.lName =
         studentData.lName[0].toUpperCase() +
         studentData.lName
@@ -124,68 +129,126 @@ function prepareObject(jsonData) {
           .toLowerCase();
     } */
 
-    //show data
-    allStudents.push(studentData);
-    return studentData;
-  });
+  //show data
 
-  displayList();
+  return studentData;
 }
 
-function displayList() {
+function displayList(st) {
   //console.log(displayList);
   //cleat the list
   document.querySelector("main").innerHTML = "";
 
   //create new list
-  allStudents.forEach(displayStudent);
+  st.forEach(displayStudent);
 }
 
-function filterButton() {
+function buttonClicked() {
   document
-    .querySelectorAll("[data-action = 'filter']")
-    .forEach((button) => button.addEventListener("click", selectFilter));
+    .querySelectorAll("[data-action='filter']")
+    .forEach((btn) => btn.addEventListener("click", filterCategory));
+
+  document
+    .querySelectorAll("[data-action='sort']")
+    .forEach((btn) => btn.addEventListener("click", sortCategory));
 }
 
-function selectFilter(event) {
+function sortCategory(event) {
+  const sort = event.target.dataset.sort;
+  console.log(sort, "?");
+
+  sortBy(sort);
+}
+
+function filterCategory(event) {
   const filter = event.target.dataset.filter;
-  console.log(`user selected: ${filter}`);
-  filterList(filter);
+  console.log(filter, "?");
+
+  filteredList(filter);
 }
 
-function filterList(stuFil) {
-  let filteredList = allStudents;
-  if (stuFil === "Slytherin") {
-    filteredList = allStudents.filter(hSlytherin);
-  } else if (stuFil === "Ravenclaw") {
-    filteredList = allStudents.filter(hRavenclaw);
+function filteredList(filteredBy) {
+  let filteredCategory = allStudents;
+  if (filteredBy === "Ravenclaw") {
+    filteredCategory = allStudents.filter(isRavenclaw);
+  } else if (filteredBy === "Gryffindor") {
+    filteredCategory = allStudents.filter(isGryffindor);
+  } else if (filteredBy === "Slytherin") {
+    filteredCategory = allStudents.filter(isSlytherin);
+  } else if (filteredBy === "Hufflepuff") {
+    filteredCategory = allStudents.filter(isHufflepuff);
+  } else {
+    console.log("bye");
   }
-  displayList(filteredList);
+
+  displayList(filteredCategory);
 }
-function hRavenclaw(studentData) {
-  console.log(hRavenclaw);
-  if (studentData.stuHouse === "Ravenclaw") {
+
+function isRavenclaw(student) {
+  return student.house === "Ravenclaw";
+}
+
+function isGryffindor(student) {
+  if (student.house === "Gryffindor") {
     return true;
   } else {
     return false;
   }
 }
-function hSlytherin(studentData) {
-  if (studentData.house === "Slytherin") {
+
+function isSlytherin(student) {
+  if (student.house === "Slytherin") {
     return true;
   } else {
     return false;
+  }
+}
+
+function isHufflepuff(student) {
+  if (student.house === "Hufflepuff") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// sort by name
+function sortBy(sortedBy) {
+  let sorted = allStudents;
+
+  if (sortedBy === "firstname") {
+    sorted = allStudents.sort(sortByFirstName);
+  } else if (sortedBy === "lastname") {
+    sorted = allStudents.sort(sortByLastName);
+  }
+
+  displayList(sorted);
+}
+
+function sortByFirstName(a, b) {
+  if (a.firstName < b.firstName) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+function sortByLastName(a, b) {
+  if (a.lastName < b.lastName) {
+    return -1;
+  } else {
+    return 1;
   }
 }
 
 function displayStudent(students) {
   //create clone
   const clone = document
-    .querySelector("template#student-list")
+    .querySelector("template#students")
     .content.cloneNode(true);
 
-  clone.querySelector(".fn").innerText = students.fName;
-  clone.querySelector(".sn").innerText = students.lName;
+  clone.querySelector(".fn").textContent = students.fName;
+  clone.querySelector(".sn").textContent = students.lName;
 
   clone
     .querySelector("article.students-name")
@@ -205,6 +268,7 @@ function showDetails(students) {
   modal.classList.remove("hide");
 }
 const close = document.querySelector(".close");
+const modal = document.querySelector(".modal-background");
 close.addEventListener("click", () => {
   modal.classList.add("hide");
 });
